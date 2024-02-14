6945095bda176e5fe0c293fd8eee054f26a34d7b
Felixxx <f3lixxx@yandex.ru>
2024-02-14 14:52:57 +0300

    change data base

--- HW_DB_5/main.cpp
+++ HW_DB_5/main.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
-#include <pqxx/pqxx>
+#include "pqxx/pqxx"
 #include "Windows.h"
 
 void create_table(pqxx::connection &con){
 	pqxx::work transaction(con);
-	
-	transaction.exec(
-		"CREATE TABLE IF NOT EXISTS client_info ("
-		"id INTEGER PRIMARY KEY,"
-		"first_name TEXT,"
-		"last_name TEXT,"
-		"email VARCHAR)");
-	transaction.exec(
-		"CREATE TABLE IF NOT EXISTS phone_number("
-		"client_id INTEGER ,"
-		"phone TEXT PRIMARY KEY, "
-	    "FOREIGN KEY (client_id) REFERENCES client_info(id) ON DELETE CASCADE)");
-	transaction.commit();
+
+		transaction.exec(
+			"CREATE TABLE IF NOT EXISTS client_info ("
+			"id SERIAL PRIMARY KEY NOT NULL,"
+			"first_name TEXT,"
+			"last_name TEXT,"
+			"email VARCHAR)");
+		transaction.exec(
+			"CREATE TABLE IF NOT EXISTS phone_number("
+			"client_id INTEGER ,"
+			"phone TEXT, "
+			"FOREIGN KEY (client_id) REFERENCES client_info(id) ON DELETE CASCADE)");
+		transaction.commit();
 }
 
 void add_client(pqxx::connection& con) {
 	pqxx::work trans(con);
 
-	// Подготовка запроса на добавление клиента
-	con.prepare("add_client", "INSERT INTO client_info(id, first_name, last_name, email) VALUES ($1, $2, $3, $4) RETURNING id");
-
-	// Подготовленное выражение для вставки номера телефона
+	con.prepare("add_client", "INSERT INTO client_info( first_name, last_name, email) VALUES ($1, $2, $3) RETURNING id");
 	con.prepare("insert_phone", "INSERT INTO phone_number(client_id, phone) VALUES($1, $2)");
 
-	std::cout << "Enter Id: ";
-	int id_cl;
-	std::cin >> id_cl;
 
 	std::cout << "Enter First Name: ";
 	std::string first_name;
@@ -44,8 +38,7 @@ void add_client(pqxx::connection& con) {
 	std::string email;
 	std::cin >> email;
 
-	// Запрос номеров телефонов для данного клиента
-		pqxx::result res = trans.exec_prepared("add_client", id_cl, first_name, last_name, email);
+		pqxx::result res = trans.exec_prepared("add_client", first_name, last_name, email);
 		int client_id = res[0]["id"].as<int>();
 
 		std::string phone_number;
@@ -95,7 +88,7 @@ void update_info(pqxx::connection& con) {
 		column_name = "email";
 		break;
 	case 4:
-		column_name = "phone_number";
+		column_name = "phone";
 		break;
 	default:
 		std::cerr << "Wrong variant" << std::endl;
@@ -104,48 +97,62 @@ void update_info(pqxx::connection& con) {
 
 	std::cout << "Enter client ID: ";
 	std::cin >> client_id;
-
-	con.prepare("update_info", "UPDATE client_info SET " + column_name + " = $1 WHERE id = $2");
+	if (num == 4) {
+		con.prepare("update_info", "UPDATE phone_number SET phone = $1 WHERE client_id = $2");
+	}
+	else {
+		con.prepare("update_info", "UPDATE client_info SET " + column_name + " = $1 WHERE id = $2");
+	}
 
 	transaction.exec_prepared("update_info", new_value, client_id); 
 	transaction.commit();
 }
 
 void delete_info(pqxx::connection& con) {
-    pqxx::work transaction(con);
-    int num;
-    int client_id;
+	pqxx::work transaction(con);
+	int num;
+	int client_id;
 
-    std::cout << "Enter what you want to do:" << std::endl;
-    std::cout << "1 - Delete user" << std::endl;
-    std::cout << "2 - Delete some info of user" << std::endl;
-    std::cin >> num;
+	std::cout << "Enter what you want to do:" << std::endl;
+	std::cout << "1 - Delete user" << std::endl;
+	std::cout << "2 - Delete some info of user" << std::endl;
+	std::cin >> num;
 
-    if (num == 1) {
-        std::cout << "Enter client ID: ";
-        std::cin >> client_id;
+	if (num == 1) {
+		std::cout << "Enter client ID: ";
+		std::cin >> client_id;
 
-        con.prepare("delete_info", "DELETE FROM client_info WHERE id = $1");
+		con.prepare("delete_user", "DELETE FROM client_info WHERE id = $1");
 
-        transaction.exec_prepared("delete_info", client_id);
-        transaction.commit();
-    } else if (num == 2) {
-        std::string column_name;
-        std::string column_value;
+		transaction.exec_prepared("delete_user", client_id);
+		transaction.commit();
+	}
+	else if (num == 2) {
+		std::cout << "Enter client ID: ";
+		std::cin >> client_id;
 
-        std::cout << "Enter client ID: ";
-        std::cin >> client_id;
+		std::cout << "Enter column name to delete info from: ";
+		std::string column_name;
+		std::cin >> column_name;
 
-        std::cout << "Enter column name to delete info from: ";
-        std::cin >> column_name;
+		if (column_name == "phone") {
+			std::string number;
+			std::cout << "Enter number what you want delete: ";
+			std::cin >> number;
 
-        con.prepare("delete_info", "UPDATE CASCADE client_info SET " + column_name + " = NULL WHERE id = $1");
+			("delete_phone", "DELETE FROM phone_number WHERE phone = $1");
+			transaction.exec_prepared("delete_phone", number);
+		}
+		else if (column_name == "first_name" || column_name == "last_name" || column_name == "email") {
+			("delete_info", "UPDATE client_info SET " + column_name + " = NULL WHERE id = $1");
+			transaction.exec_prepared("delete_info", client_id);
+		}
 
-        transaction.exec_prepared("delete_info", client_id);
-        transaction.commit();
-    } else {
-        std::cout << "Wrong option" << std::endl;
-    }
+		transaction.commit();
+	}
+	else {
+		std::cout << "Wrong option" << std::endl;
+	}
 }
 
 void find_info(pqxx::connection& con) {
@@ -191,7 +198,7 @@ void find_info(pqxx::connection& con) {
 
 	pqxx::result res = transaction.exec_prepared("find_info", search_value);
 
-	// Вывод результатов запроса
+	// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 	for (const auto& row : res) {
 		std::cout << "ID: " << row["id"].as<int>() << std::endl;
 		std::cout << "First Name: " << row["first_name"].as<std::string>() << std::endl;
